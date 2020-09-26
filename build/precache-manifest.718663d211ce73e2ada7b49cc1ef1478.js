5414a2d74db800fd47253ea516bd75d515c2dfaa
kerolos <kerolos.sss@gmail.com>
2020-09-26 23:52:18 +0200

    adding build to puplish

---
+++ build/precache-manifest.718663d211ce73e2ada7b49cc1ef1478.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e60bb106a006bb3b44b0c68bf5a1777c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e83f64e26ba759d9d15",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "312d43ff18c51134a5af",
+    "url": "/static/js/2.3057ce6a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3057ce6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e83f64e26ba759d9d15",
+    "url": "/static/js/main.cecc0c3f.chunk.js"
+  },
+  {
+    "revision": "c0acef1f984a4e940018",
+    "url": "/static/js/runtime-main.693ddd72.js"
+  }
+]);
